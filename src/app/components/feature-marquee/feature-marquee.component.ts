dd6a33641f9c3a71f1f05ecb701ec54bed3a5d88
Alex Simpson <alexkiwilson@gmail.com>
2022-11-29 20:23:02 -0500

    marquee building

---
+++ src/app/components/feature-marquee/feature-marquee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-marquee',
+  templateUrl: './feature-marquee.component.html',
+  styleUrls: ['../../../styles.css']
+})
+export class FeatureMarqueeComponent {
+
+}
